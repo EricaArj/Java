30e3db68e476cc4b1c1368403f18767e4a16ca98
EricaArj <erika.f55@hotmail.com>
2023-02-15 20:51:34 -0300

    Aula05

--- aula-005/src/Exeo1Set.java
+++ aula-005/src/Exeo1Set.java
@@ -1,38 +1,30 @@
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Exeo1Set {
 
 	public static void main(String[] args) {
-		
+		ArrayList<Integer> valores = new ArrayList<Integer>();
 		Scanner leia = new Scanner(System.in);
-		Set<Integer> valores = new HashSet<Integer>();
+		int testeNu;
 		
 		valores.add(2);
 		valores.add(5);
-		valores.add(10);
+		valores.add(1);
 		valores.add(3);
 		valores.add(4);
-		valores.add(2);
-		valores.add(2);
-		valores.add(3);
+		valores.add(9);
+		valores.add(7);
+		valores.add(8);
 		valores.add(10);
-		valores.add(15);
+		valores.add(6);
 		
-		int consulta;
+		System.out.println("Entre com o número para saber se está na lista: ");
+		testeNu = leia.nextInt();
 		
-		System.out.println("Listando todas as valores: ");
-		consulta = leia.nextInt();
-		valores.forEach(System.out::println);//lup
-		
-		
-		valores.forEach(System.out::println);//lup
-		System.out.println("Nota 7 exixte: " + valores.contains(valores));
-		
-		for(var nota : valores)
-			System.out.println(nota.hashCode());
-
+		if(valores.contains(testeNu))
+			System.out.println("O número " + testeNu + " foi encontrado!");
+		else
+			System.out.println("O número " + testeNu + " Não foi encontrado!");
 	}
-
 }
